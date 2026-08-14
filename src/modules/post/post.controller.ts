132b8ebb1c23b664f340e5aa842febb14563820b
younus-always <younusofficial443@gmail.com>
2026-08-14 23:25:25 +0600

    feat: retrieve all post filtering with multiple value

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PostService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
       try {
@@ -30,7 +31,21 @@ const getAllPost = async (req: Request, res: Response) => {
       try {
             const { search } = req.query;
             const searchString = typeof search === "string" ? search : undefined;
-            const result = await PostService.getAllPost({ search: searchString });
+            const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+            // true or false
+            const isFeatured = req.query.isFeatured
+                  ? req.query.isFeatured === "true"
+                        ? true
+                        : req.query.isFeatured === "false"
+                              ? false
+                              : undefined
+                  : undefined;
+
+            const status = req.query.status as PostStatus | undefined;
+            const userId = req.query.userId as string | undefined;
+
+            const result = await PostService.getAllPost({ search: searchString, tags, isFeatured, status ,userId});
+
             res.status(200).json({
                   success: true,
                   statusCode: 200,
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,5 @@
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
+import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (data: Omit<Post, 'id' | 'userId' | 'createdAt' | 'updatedAt'>, userId: string) => {
@@ -11,28 +12,70 @@ const createPost = async (data: Omit<Post, 'id' | 'userId' | 'createdAt' | 'upda
       return result;
 };
 
-const getAllPost = async (payload: { search: string | undefined }) => {
-      const allPost = await prisma.post.findMany({
-            where: {
+const getAllPost = async ({ search, tags, isFeatured, status, userId }: {
+      search: string | undefined,
+      tags: string[] | [],
+      isFeatured: boolean | undefined,
+      status: PostStatus | undefined,
+      userId: string | undefined,
+}) => {
+
+      const andConditions: PostWhereInput[] = [];
+
+      if (search) {
+            andConditions.push({
                   OR: [
                         {
                               title: {
-                                    contains: payload.search as string,
+                                    contains: search as string,
                                     mode: "insensitive"
                               }
                         },
                         {
                               content: {
-                                    contains: payload.search as string,
+                                    contains: search as string,
                                     mode: "insensitive"
                               }
                         },
                         {
                               tags: {
-                                    has: payload.search as string
+                                    has: search as string
                               }
                         }
                   ]
+            })
+      };
+
+      if (tags.length > 0) {
+            andConditions.push({
+                  tags: {
+                        hasEvery: tags
+                  }
+            })
+      };
+
+      if (typeof isFeatured === "boolean") {
+            andConditions.push({
+                  isFeatured
+            })
+      };
+
+      if (status) {
+            andConditions.push({
+                  status
+            })
+      };
+
+      if (userId) {
+            andConditions.push({
+                  userId
+            })
+      };
+
+
+      const allPost = await prisma.post.findMany({
+            where: {
+                  AND: andConditions
             }
       });
       return allPost;
